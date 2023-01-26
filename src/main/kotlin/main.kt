c1870768bf94efb774a1e6c0fa55be3c56eefff6
Brendan O'Connor <20098888@wit.ie>
2023-01-26 16:38:56 +0000

    Changed payslip to use a single set of triple quotation marks

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,4 @@
+import java.math.BigDecimal
 import java.math.RoundingMode
 import java.text.DecimalFormat
 
@@ -15,24 +16,29 @@ fun main(args: Array<String>){
     val df = DecimalFormat("#.##")
     df.roundingMode = RoundingMode.CEILING
     println("Pay Slip Printer")
-    println(
-                "_____________________________________________________________________\n" +
-                "|                          Monthly Payslip                          |\n" +
-                "|___________________________________________________________________|\n" +
-                "|                                                                   |\n" +
-                "|       Employee Name: ${fname.uppercase()} ${sname.uppercase()} (${gender})    Employee ID: ${empid}            |\n" +
-                "|                                                                   |\n" +
-                "|___________________________________________________________________|\n" +
-                "|                                                                   |\n" +
-                "|       Payment Details                Deduction Details            |\n" +
-                "|___________________________________________________________________|\n" +
-                "|       Salary: ${df.format(salary/12)}                PAYE: ${df.format((salary/12)*(paye/100))}                |\n" +
-                "|       Bonus: ${df.format(bonus/12)}                  PRSI: ${df.format((salary/12)*(prsi/100))}                 |\n" +
-                "|                                      Cycle To Work: ${df.format(deduction)}         |\n" +
-                "|___________________________________________________________________|\n" +
-                "|       Gross: ${df.format((salary + bonus)/12)}                 Total Deductions: ${df.format((deduction+salary*(paye/100)+salary*(prsi/100))/12)}    |\n" +
-                "|___________________________________________________________________|\n" +
-                "|                         Net Pay: ${df.format(((salary+bonus)-(deduction+salary*(paye/100)+salary*(prsi/100)))/12)}                          |\n" +
-                "|___________________________________________________________________|\n"
+    println("""
+                _____________________________________________________________________
+                |                          Monthly Payslip                          |
+                |___________________________________________________________________|
+                |                                                                   |
+                |       Employee Name: ${fname.uppercase()} ${sname.uppercase()}(${gender.uppercase()})     Employee ID: ${empid}            |
+                |                                                                   |
+                |___________________________________________________________________|
+                |                                                                   |
+                |       Payment Details                Deduction Details            |
+                |___________________________________________________________________|
+                |       Salary: ${df.format(salary/12)}                PAYE: ${df.format((salary/12)*(paye/100))}                |
+                |       Bonus: ${df.format(bonus/12)}                  PRSI: ${df.format((salary/12)*(prsi/100))}                 |
+                |                                      Cycle To Work: ${df.format(deduction)}         |
+                |___________________________________________________________________|
+                |       Gross: ${df.format((salary + bonus)/12)}                 Total Deductions: ${df.format((deduction+salary*(paye/100)+salary*(prsi/100))/12)}    |
+                |___________________________________________________________________|
+                |                         Net Pay: ${df.format(((salary+bonus)-(deduction+salary*(paye/100)+salary*(prsi/100)))/12)}                          |
+                |___________________________________________________________________|
+                """
     )
+}
+
+fun twodecimal(input: Double){
+    val short = BigDecimal(input).setScale(2, RoundingMode.HALF_EVEN)
 }
