0ce62cec9f6d674e3e0bf9ad376e314fc4e71f91
Brendan O'Connor <20098888@wit.ie>
2023-01-31 10:12:22 +0000

    Added start of menu, adjusted fullname function to include title

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -10,37 +10,58 @@ val prsi = 5.2
 val bonus = 1450.50
 val deduction = 54.33
 
+var title = ""
+
 fun monthlySal() = roundTwoDecimals(salary / 12)
 fun monthlyBonus() = roundTwoDecimals(bonus / 12)
-fun fullName() = "${fname} ${sname}"
+fun fullName() =  when (gender) {
+    "m", "M" -> "Mr. $fname $sname"
+    "f", "F" -> "Ms. $fname $sname"
+    else -> "$fname $sname"
+}
 fun monthlyPrsi() = roundTwoDecimals((salary/12)*(prsi/100))
 fun monthlyPaye() = roundTwoDecimals((salary/12)*(paye/100))
 fun grossSalary() = roundTwoDecimals(((salary + bonus)/12))
 fun totalDeduction() = roundTwoDecimals(((deduction+salary*(paye/100)+salary*(prsi/100))/12))
 fun netPay() = roundTwoDecimals((((salary+bonus)-(deduction+salary*(paye/100)+salary*(prsi/100)))/12))
+fun getPayslip() = """
+      _____________________________________________________________________
+                                 Monthly Payslip                           
+      _____________________________________________________________________
+                                                                           
+              Employee Name: ${fullName()}(${gender.uppercase()})     Employee ID: ${empid}             
+                                                                           
+      _____________________________________________________________________
+                                                                           
+              Payment Details                Deduction Details             
+      _____________________________________________________________________
+             Salary: ${monthlySal()}                 PAYE: ${monthlyPaye()}                 
+              Bonus: ${monthlyBonus()}                  PRSI: ${monthlyPrsi()}                  
+                                             Cycle To Work: ${(deduction)}          
+      _____________________________________________________________________
+              Gross: ${grossSalary()}                 Total Deductions: ${totalDeduction()}     
+      _____________________________________________________________________
+                                Net Pay: ${netPay()}                           
+      _____________________________________________________________________
+      """
+fun menu() : Int {
+    print("""
+         Employee Menu for ${fullName()}
+           1. Monthly Salary
+           2. Monthly PRSI
+           3. Monthly PAYE
+           4. Monthly Gross Pay
+           5. Monthly Total Deductions
+           6. Monthly Net Pay
+           7. Full Payslip
+           8. Exit
+         Enter Option : 
+         """)
+    return readLine()!!.toInt()
+}
+
 fun main(args: Array<String>){
-    println("Pay Slip Printer")
-    println("""
-                _____________________________________________________________________
-                |                          Monthly Payslip                          |
-                |___________________________________________________________________|
-                |                                                                   |
-                |       Employee Name: ${fullName()}(${gender.uppercase()})     Employee ID: ${empid}            |
-                |                                                                   |
-                |___________________________________________________________________|
-                |                                                                   |
-                |       Payment Details                Deduction Details            |
-                |___________________________________________________________________|
-                |       Salary: ${monthlySal()}                 PAYE: ${monthlyPaye()}                |
-                |       Bonus: ${monthlyBonus()}                  PRSI: ${monthlyPrsi()}                 |
-                |                                      Cycle To Work: ${(deduction)}         |
-                |___________________________________________________________________|
-                |       Gross: ${grossSalary()}                 Total Deductions: ${totalDeduction()}    |
-                |___________________________________________________________________|
-                |                         Net Pay: ${netPay()}                          |
-                |___________________________________________________________________|
-                """
-    )
+    println(getPayslip())
 }
 
 fun roundTwoDecimals(number: Double) = round(number * 100) / 100
